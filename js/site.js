1520b0ef2140ce7f6066daab8beb7b0ece18f49a
Ningyu-Zhang98 <108341745+Ningyu-Zhang98@users.noreply.github.com>
2022-08-16 21:34:25 +0800

    Initial Commit
    Functionalities are done for Rewind (reverse strings)

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-//Call hello world
-function HelloWorld(){
-    alert('hello world');
+//Get the string from the page
+
+//Controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+
+//Reverse the string
+//Logic functions
+function reverseString(uString){
+    let revString = [];
+    
+    for(let index = uString.length-1; index>-1; index--){
+        revString+=uString[index]
+    }
+    return revString
+}
+
+
+//Display the reversed string
+//View functions
+function displayString(revString){
+    //Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}` 
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
